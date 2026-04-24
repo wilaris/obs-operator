dcd1a58b0f5d68663c00ff54b32aedf8e48e9412
Marcel Kirschner <mk@wilaris.de>
2026-04-24 21:37:22 +0200

    Remove ProviderConfig short name
    Drop the `pc` short name from the ProviderConfig CRD because it collides
    with the built-in PriorityClass alias.

--- api/v1alpha1/providerconfig_types.go
+++ api/v1alpha1/providerconfig_types.go
@@ -63,7 +63,7 @@ type ProviderConfigStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:scope=Namespaced,shortName=pc
+// +kubebuilder:resource:scope=Namespaced
 // +kubebuilder:printcolumn:name="Region",type=string,JSONPath=".spec.region"
 // +kubebuilder:printcolumn:name="Ready",type=string,JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="Age",type=date,JSONPath=".metadata.creationTimestamp"
